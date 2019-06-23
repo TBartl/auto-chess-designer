a7e565f12b13141f2443db21c83d95310aeb44f6
Thomas Bartlett <tbartl@umich.edu>
2019-06-22 21:33:05 -0400

    Show synergy icons

--- src/components/PiecesDisplay.vue
+++ src/components/PiecesDisplay.vue
@@ -3,7 +3,10 @@
   <div class=" m-2 select position-relative hover-reveal" 
       v-for="(piece, index) in sortedPieces" :key="index">
     <router-link :to="`/piece/${piece.name}`" class="d-flex align-items-center flex-column">
-      <img :src="piece.image" :alt="piece.name" :style="getImageStyle(piece)">
+      <div class="position-relative">
+        <span class="synergies">{{getSynergyIcons(piece)}}</span>
+        <img :src="piece.image" :alt="piece.name" :style="getImageStyle(piece)">
+      </div>
       <p>{{piece.name}}</p>
     </router-link>
     <div v-if="hasActions" class="delete-piece reveal" @click="deletePiece(piece)">❌</div>
@@ -18,9 +21,11 @@
 <script>
 import { generateUID } from "@/utils";
 import CONSTANTS from "@/constants";
+import { mapState } from "vuex";
 export default {
   props: ["hasActions", "pieces"],
   computed: {
+    ...mapState(["schema"]),
     canCreateNew() {
       return !this.pieces.find(piece => piece.name == CONSTANTS.NEW_PIECE.name);
     },
@@ -47,6 +52,26 @@ export default {
       return {
         border: "5px solid " + borderColor
       };
+    },
+    getSynergyIcons(piece) {
+      // Get the real synergies from the ids stored in the piece
+      var realSynergies = piece.synergies.map(synergyID => {
+        return this.schema.synergies.find(synergy => synergy.id == synergyID);
+      });
+
+      // Remove undefined synergies
+      realSynergies = realSynergies.filter(synergy => synergy);
+
+      // Sort in the order that the synergies are listed on the synergy page
+      var sortedSynergies = realSynergies.sort((a, b) => {
+        var aIndex = this.schema.synergies.indexOf(a);
+        var bIndex = this.schema.synergies.indexOf(b);
+        return aIndex - bIndex;
+      });
+
+      // Get the first character from all the synergies 
+      var icons = sortedSynergies.map(synergy => synergy.name.substring(0, 2));
+      return icons.join("");
     }
   }
 };
@@ -62,5 +87,16 @@ export default {
   background-color: #252c50;
   box-shadow: inset 0 5px 10px rgba(0, 0, 0, 0.1),
     inset 0 15px 40px rgba(0, 0, 0, 0.05);
+}
+.synergies {
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  bottom: 4px;
+  color: white !important;
+  text-align: center;
+  font-size: 14px;
 }
 </style>
